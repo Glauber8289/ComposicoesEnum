93342a7d8b8be378896da7a77bc169cfb42c2277
Glauber Peterson <Petersonglauber@hotmail.com>
2023-01-08 23:43:18 -0300

    Acrescentando entidades Hourcontract,Worker,WorkerLevel e criando a funcao Program

--- src/aplication/Program.java
+++ src/aplication/Program.java
@@ -1,11 +1,30 @@
 package aplication;
 
+import java.util.Scanner;
+
+import entities.Department;
+import entities.Worker;
+import entities.Enum.WorkerLevel;
+
 public class Program {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		// obtendo dados do trabalhador 
+       Scanner sc = new Scanner(System.in);
+       System.out.print("Digite seu departamento");
+       String departmentName = sc.next();
+       System.out.print("Dados do trabalhador");
+       System.out.print("Digite seu nome");
+       String workername= sc.next();
+       System.out.print("Digite seu  nivel");
+       String workerLevel= sc.next();
+       System.out.print("Digite seu Salario");
+       double baseSalary= sc.nextDouble();
        
-        		
+       //instanciando trabalhador 
+       Worker worker = new Worker(workername, WorkerLevel.valueOf(workerLevel), baseSalary, new Department(departmentName));
+       
+       sc.close();   		
 	}
-
+    
 }
